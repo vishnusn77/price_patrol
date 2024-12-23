711acae2ee1a92d8c8f7504644157b9ff1e7cde6
Vishnu Sreekumaran Nair <200557136@student.georgianc.on.ca>
2024-12-23 00:39:03 -0500

    update settings.py

--- price_tracker/settings.py
+++ price_tracker/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker',
-    'django_cron',
 ]
 
 MIDDLEWARE = [
@@ -90,7 +89,6 @@ DATABASES = {
         }
     }
 }
-
 
 
 # Password validation
